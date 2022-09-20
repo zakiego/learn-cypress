5c03d17423e28b92fd50547337d5156f9a6474d1
zakiego <belummikir@gmail.com>
2022-09-20 20:02:24 +0800

    test: init

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,17 @@
+describe("home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("the h1 contains the correct text", () => {
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  it("the feature of the homepage are correct text", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  })
+})
