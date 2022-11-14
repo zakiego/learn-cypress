744f8de5e4c44288841c724865349ef710724999
zakiego <belummikir@gmail.com>
2022-11-14 08:38:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Real World Testing with Cypress - Course App
+
+This application is for the [Testing your first application course](https://learn.cypress.io/testing-your-first-application) on [learn.cypress.io](https://learn.cypress.io/)
+
+You can find the instructions for this course [here](https://learn.cypress.io/testing-your-first-application/course-app).
---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,19 @@
+describe("home spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("passes", () => {
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress"
+    )
+  })
+
+  it("the features on the homepage are correct", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  })
+})
+
+export {}
